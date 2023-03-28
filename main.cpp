7e76cfd5017e1c9f1848cf364fdd792b89f7e92a
ATARI2341 <aaronalejandroochoaneri@gmail.com>
2023-03-27 23:16:12 -0600

    algoritmo de ordenamiento GNOME

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include<iostream>
 using namespace std;
-
+// Algoritmo de ordenamiento GNOME
 int main(){
+// i es la variable de control tmp es el pibote para cambiar datos de lugar
+int i=0,tmp=0;
 
-/*Este programa es desarrollado para encriptar archivos de contenido delicado*/
-string ruta="",extencion="";
-int opc=0;
-cout<<"Elige la ruta de los archivos a encriptar"<<endl;
-cout<<"¿Desea algun tipo de archivo en espesifico?"<<endl<<"1 si"<<"cualquier numero no"<endl;
-cin>>opc;
-if(opc==1){
-count<<"encriptar archivos espesificos"<<endl;
-cin>>extencion;
+//a es el arreglo
+int a[10] = {2,54,3,2,6,7,4,2,1,23};
+
+//el numero 9 en el while reprecenta el "tamaño del arreglo-1"
+while (i<=9){
+
+    if(a[i-1]<=a[i]){
+        i++;
+    }
+
+    else {
+        tmp=a[i-1];
+        a[i-1]=a[i];
+        a[i]=tmp;
+        i--;
+    }
+    if(i==0){
+        i=1;
+    }
+};
+for(int j=0;j<9;j++){
+    cout<<a[j]<<" , ";
 }
 
 return 0;
