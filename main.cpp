5a35ac2feae605bfcce25d472ef459a7f99fa17c
ATARI2341 <aaronalejandroochoaneri@gmail.com>
2023-03-27 22:56:38 -0600

    segundo pos nomas

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main(){
 
-cout<<"holaaaaa enfermera"<<endl;
+/*Este programa es desarrollado para encriptar archivos de contenido delicado*/
+string ruta="",extencion="";
+int opc=0;
+cout<<"Elige la ruta de los archivos a encriptar"<<endl;
+cout<<"¿Desea algun tipo de archivo en espesifico?"<<endl<<"1 si"<<"cualquier numero no"<endl;
+cin>>opc;
+if(opc==1){
+count<<"encriptar archivos espesificos"<<endl;
+cin>>extencion;
+}
 
 return 0;
 }
